0060d0b4b099ce9b0bd33357b7b70cb06ad895ad
JasonSurendran <surendranjason@gmail.com>
2020-05-16 11:11:53 -0400

    Setup icons and images

--- new_app/lib/main.dart
+++ new_app/lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/rendering.dart';
 import 'package:new_app/constant.dart';
+import 'package:flutter_svg/flutter_svg.dart';
 
 void main() {
   runApp(MyApp());
@@ -31,22 +32,63 @@ class HomeScreen extends StatelessWidget {
     return Scaffold(
       body: Column(
         children: <Widget>[
-          Container(
-            height: 350,
-            width: double.infinity,
-            decoration: BoxDecoration(
-              gradient: LinearGradient(
-                begin: Alignment.topRight,
-                end: Alignment.bottomLeft,
-                colors: [
-                  Color(0xFF3383CD),
-                  Color(0xFF11249F),
+          ClipPath(
+            clipper: MyClipper(),
+            child: Container(
+              padding: EdgeInsets.only(left: 40, top:50, right:20),
+              height: 350,
+              width: double.infinity,
+              decoration: BoxDecoration(
+                gradient: LinearGradient(
+                  begin: Alignment.topRight,
+                  end: Alignment.bottomLeft,
+                  colors: [
+                    Color(0xFF3383CD),
+                    Color(0xFF11249F),
+                  ],
+                ),
+                image: DecorationImage(
+                  image:AssetImage(
+                    'assets/images/virus.png'
+                  ),
+                ),
+              ),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: <Widget>[
+                  Align(
+                  alignment: 
+                  Alignment.topRight,
+                  child:
+                  SvgPicture.asset(
+                  "assets/icons/menu.svg"
+                  ),
+                ),
                 ],
-              )
-            )
+              ),
+            ),
           )
         ],
       ),
     );
   }
+}
+
+class MyClipper extends CustomClipper<Path>{
+  @override
+  Path getClip(Size size) {
+    var path = Path();
+    path.lineTo(0, size.height - 80);
+    path.quadraticBezierTo(
+      size.width/2, size.height, size.width, size.height-80);
+    path.lineTo(size.width, 0);
+    path.close();
+    return path;
+    }
+  
+    @override
+    bool shouldReclip(CustomClipper<Path> oldClipper) {
+      return false;
+  }
+
 }
